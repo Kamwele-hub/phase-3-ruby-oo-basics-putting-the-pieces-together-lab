f0e918abfbe5ba8ff46c91ad27ff14402562d1a4
Stephen Kamwele <stephen.kamwele@student.moringaschool.com>
2023-05-18 09:32:30 +0300

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,23 @@
+class Book
 
+    attr_accessor :title, :author, :page_count, :genre, :turn_page
+
+    def initialize(title)
+        @title = title
+    end 
+
+    # def author_name(author)
+    #     @author = author
+    # end 
+
+    # def page_counter(page_count)
+    #     @page_count = page_count
+    #     page_count += 1
+    # end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end 
+
+end 
 
